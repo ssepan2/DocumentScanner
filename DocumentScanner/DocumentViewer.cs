21d0bfa65524fbc55d09915d9968ffb22b70161a
Stephen Sepan <sjsepan@outlook.com>
2020-12-03 14:05:17 -0500

    fix null reference in viewmodel when assigning status message during file exit

--- DocumentScanner/DocumentViewer.cs
+++ DocumentScanner/DocumentViewer.cs
@@ -123,7 +123,7 @@ namespace DocumentScanner
                 else if (e.PropertyName == "StatusMessage")
                 {
                     //replace default action by setting control property
-                    StatusBarStatusMessage.Text = ViewModel.StatusMessage;
+                    StatusBarStatusMessage.Text = (ViewModel != null ? ViewModel.StatusMessage : (String)null);
                     //e = new PropertyChangedEventArgs(e.PropertyName + ".handled");
 
                     //ConsoleApplication.defaultOutputDelegate(String.Format("{0}", StatusMessage));
@@ -131,7 +131,7 @@ namespace DocumentScanner
                 else if (e.PropertyName == "ErrorMessage")
                 {
                     //replace default action by setting control property
-                    StatusBarErrorMessage.Text = ViewModel.ErrorMessage;
+                    StatusBarErrorMessage.Text = (ViewModel != null ? ViewModel.ErrorMessage : (String)null);
                     //e = new PropertyChangedEventArgs(e.PropertyName + ".handled");
 
                     //ConsoleApplication.defaultOutputDelegate(String.Format("{0}", ErrorMessage));
